9cb31f191a38da7a1a4672232bded1f6958c884a
8b1tz <yohannaoliveira12@gmail.com>
2021-04-24 03:38:10 -0300

    first commit

---
+++ src/app/shared/model/USUARIOS.ts
@@ -0,0 +1,20 @@
+export const USUARIOS = [
+  {
+    nome: 'Gustavo',
+    cpf: '123',
+    idade: 34,
+    telefone: 99999999
+  },
+  {
+    nome: 'Wagner',
+    cpf: '124',
+    idade: 32,
+    telefone: 9888888888
+  },
+  {
+    nome: 'Diniz',
+    cpf: '125',
+    idade: 30,
+    telefone: 98888888887
+  }
+];
